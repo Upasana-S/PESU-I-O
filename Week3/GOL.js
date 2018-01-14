5497db754644ed85cc3bf5f68215763b04242a50
Upasana-S <upasanasridhared@gmail.com>
2018-01-14 20:45:50 +0530

    Add files via upload

---
+++ Week3/GOL.js
@@ -0,0 +1,40 @@
+
+
+function GOL() {
+
+  this.w = 8;
+  this.columns = width/this.w;
+  this.rows = height/this.w;
+
+  // Game of life board
+  this.board = new Array(this.columns);
+  
+  }
+
+  // initialise the board with some live cells
+  this.init = function() {
+  
+  };
+  this.init();
+
+  // The process of creating the new generation
+  this.generate = function() {
+
+
+    // Loop through every spot in our 2D array and check spots neighbors
+    
+
+        // Add up all the states in a 3x3 surrounding grid
+    
+
+        // Rules of Life
+      
+        // else do nothing!
+   
+  };
+
+  // This is the easy part, just draw the cells, fill 255 for '1', fill 0 for '0'
+  this.display = function() {
+   
+  };
+}
