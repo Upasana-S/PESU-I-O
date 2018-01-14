55efea0b84f303e55d1586d5b395e6816099a0db
Upasana-S <upasanasridhared@gmail.com>
2018-01-14 18:02:57 +0530

    Update sketch.js

--- Week2/Vector_object/sketch.js
+++ Week2/Vector_object/sketch.js
@@ -7,6 +7,7 @@ let velocity;
 function setup() {
   createCanvas(640, 360);
   background(255);
+  // format to access elements of a vector: vector.x and vector.y
   position = createVector(100, 100);
   velocity = createVector(2.5, 5);
 }
@@ -17,16 +18,11 @@ function draw() {
   // Add the current speed to the position.
   position.add(velocity);
 
-  if ((position.x > width) || (position.x < 0)) {
-    velocity.x = velocity.x * -1;
-  }
-  if ((position.y > height) || (position.y < 0)) {
-    velocity.y = velocity.y * -1;
-  }
+// write code to update velocity by adding acceleration. 
 
   // Display circle at x position
   stroke(0);
   strokeWeight(2);
   fill(127);
-  ellipse(position.x, position.y, 48, 48);
+  ellipse(__, __, 48, 48);
 }
