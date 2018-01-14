18762fbb1159a560d1731cdce2573b173c58d4f4
Upasana-S <upasanasridhared@gmail.com>
2018-01-14 18:22:52 +0530

    Create ball.js

---
+++ Week2/Class_object/ball.js
@@ -0,0 +1,20 @@
+class Ball {
+  constructor() {
+    this.position 
+    this.velocity
+  }
+  
+  collision(){
+    // handle the case where objects to collide
+  }
+  update() {
+    // Add the current speed to the position.
+   //update speed
+  }
+  display() {
+    // Display circle at x position
+    stroke(0);
+    fill(175);
+    ellipse(__,__, 48, 48);
+  }
+}
